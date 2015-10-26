1391943a9f4dff79af105e72b730171515bd9914
diegomatus <cube.di.rubik@gmail.com>
2015-10-25 20:32:38 -0600

    Static production

--- caafi_uv/wsgi.py
+++ caafi_uv/wsgi.py
@@ -13,9 +13,9 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "caafi_uv.settings")
 
-#Heroku's config
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+application = get_wsgi_application()
 
+#Heroku's config
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
