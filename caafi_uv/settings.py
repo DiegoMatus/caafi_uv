24f2a044e67fda6af35848add244dc6f41bccbb8
diegomatus <cube.di.rubik@gmail.com>
2015-10-25 20:54:07 -0600

    Static production

--- caafi_uv/settings.py
+++ caafi_uv/settings.py
@@ -102,8 +102,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Media files (images, files upload for user)
 MEDIA_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '../media/').replace('\\', '/'))
