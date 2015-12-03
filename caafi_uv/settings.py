1c11b5c8f273ac439f93d68b6eafb30cce64f4df
diegomatus <cube.di.rubik@gmail.com>
2015-12-02 22:52:31 -0600

    debe servir

--- caafi_uv/settings.py
+++ caafi_uv/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '../static/').replace('\\', '/'))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
