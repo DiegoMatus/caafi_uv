d6ea350d187fa334c01d88c5b2db96fae927c051
diegomatus <cube.di.rubik@gmail.com>
2015-12-02 21:56:59 -0600

    Static problems

--- caafi_uv/settings.py
+++ caafi_uv/settings.py
@@ -103,9 +103,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '../static/').replace('\\', '/'))
+#STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '../static/').replace('\\', '/'))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '../media/').replace('\\', '/'))
 
