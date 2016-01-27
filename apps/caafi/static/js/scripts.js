2a028f3a47a87749ff611eb8ba4abf030c7596ad
diegomatus <cube.di.rubik@gmail.com>
2016-01-26 22:19:13 -0600

    feat: Change color categorie

--- apps/caafi/static/js/scripts.js
+++ apps/caafi/static/js/scripts.js
@@ -1,4 +1,4 @@
-var colors = ['#1AAEDF', '#CA53CE', '#D91B5B', '#F0992A', '#F7D947', '#8DC74D', '#F0DEA4', '#F9C8A7', '#85235B'];
+var colors = ['#1AAEDF', '#76678B', '#D91B5B', '#F0992A', '#F7D947', '#8DC74D', '#F0DEA4', '#F9C8A7', '#85235B'];
 
 	var i = 0;
 	$('.category-content').each(function(){
